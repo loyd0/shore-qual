7c24995224c3a1ac157e31c25e904e3282bdc32b
loyd0 <samloyd@hotmail.co.uk>
2020-06-17 11:14:59 +0100

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,12 @@ import Layout from "../components/layout/Layout"
 import SEO from "../components/SEO"
 
 const IndexPage = () => (
+  <>
+  <SEO title="Home" />
   <Layout>
-    <SEO title="Home" />
+      {/* Content goes here */}
   </Layout>
+  </>
 )
 
 export default IndexPage

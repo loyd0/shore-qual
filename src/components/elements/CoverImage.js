05e2b3f3911b3bbfd0939537b64dace168516c05
loyd0 <samloyd@hotmail.co.uk>
2020-11-26 13:49:02 +0400

    added animation

--- src/components/elements/CoverImage.js
+++ src/components/elements/CoverImage.js
@@ -7,7 +7,7 @@ export default function CoverImage({ image, alt, overlay, className, children })
             className={`w-full h-full lg:min-h-screen  relative ${className} `}>
             { children }
             {overlay &&  <div className={`${overlay} w-full h-full absolute top-0 z-10`} />}   
-           <Image style={{ position: "absolute" }} className="absolute top-0 w-full h-full z-0" image={image} alt={alt}/>
+           <Image style={{ position: "absolute" }} className="absolute  top-0 w-full h-full z-0" image={image} alt={alt}/>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,21 @@ import { AnchorLink } from "gatsby-plugin-anchor-links";
 import ContentBlock from "../components/contentful-elements/ContentBlock";
 import Section from '../components/contentful-elements/Section';
 import { useBlocks } from '../utils/useBlocks';
+import Lottie from 'react-lottie';
+import shoreAnimation from '../animations/shore.json'
+
+
+const shoreAnimationOptions = {
+  loop: true,
+  autoplay: true, 
+  animationData: shoreAnimation,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+  }
+};
+
+
+
 
 const IndexPage = ({ data }) => {
 
@@ -28,10 +43,20 @@ const IndexPage = ({ data }) => {
       <SEO title={title} />
 
       <section className="relative">
-        <CoverImage image={coverPhoto} alt={coverPhoto.title} overlay="bg-gray-900 bg-opacity-20" className="-mt-20" >
+        <CoverImage
+        style={{ objectPosition: "90%"}}
+        image={coverPhoto} alt={coverPhoto.title} overlay="bg-gray-900 bg-opacity-20" className="-mt-20" >
           <div className="flex h-screen w-full top-0 items-center px-6 text-white  text-shadow">
-            <div className="flex flex-col text-center w-full relative z-40 space-y-6 ">
-              {header && <h2 className="text-5xl font-bold ">{header}</h2>}
+            <div className="flex flex-col text-center w-full relative z-front space-y-6 ">
+
+              <div className="max-w-2xl mx-auto">
+              <Lottie 
+                options={shoreAnimationOptions}
+                width={"100%"}
+                />
+              </div>
+            
+              {header && <h2 className="lg:text-5xl font-bold ">{header}</h2>}
               {subHeader && <h4>{subHeader}</h4>}
               {scrollText && <AnchorLink to="#intro" className="w-48 mx-auto ">
                 <p className="leading-tight">{scrollText}</p>

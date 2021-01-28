19e9ae091ce6701fbc1b1de4599f49e8c8b92519
loyd0 <samloyd@hotmail.co.uk>
2021-01-28 21:09:15 +0000

    added mobile hiding

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,57 +17,57 @@ export default function About({
   const [animating, setAnimating] = React.useState(false)
 
 
-    const container = {
-        hidden: { opacity: 1 },
-        show: {
-            opacity: 1,
-            transition: {
-                staggerChildren: 4,
-                repeat: Infinity
-            },
-        },
-        onAnimationComplete: () => console.log("0"),
-    }
+  const container = {
+    hidden: { opacity: 1 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 4,
+        repeat: Infinity
+      },
+    },
+    onAnimationComplete: () => console.log("0"),
+  }
 
-    const siteAnimation = {
-        hidden: {
-            opacity: 0
-        },
-        show: {
-            y: [20, 0, 20],
-            opacity: [0, 1, 0],
-            transition: {
-                duration: 8,
-                ease: "easeInOut",
-            },
-        },
-        onAnimationComplete: () => console.log("1"),
-    }
+  const siteAnimation = {
+    hidden: {
+      opacity: 0
+    },
+    show: {
+      y: [20, 0, 20],
+      opacity: [0, 1, 0],
+      transition: {
+        duration: 8,
+        ease: "easeInOut",
+      },
+    },
+    onAnimationComplete: () => console.log("1"),
+  }
 
   return <Layout
   >
     <SEO title={title} />
     <section className="relative">
-      <div className="absolute top-0 w-full ">
+      <div className="absolute top-0 w-full max-w-screen-xl mx-auto ">
         <Image className="w-full xl:w-5/6 ml-auto" image={allImageSharp.nodes[0]} alt="Map of Great British Isles " />
         <motion.div
-                    variants={container}
-                    initial="hidden"
-                    animate="show"
-                    onAnimationComplete={() => setAnimating(!animating)}
-
-                >
-                    {
-                        Location.sort(() => Math.random() - 0.5).map(({ location, header, description}) => {
-                        return  <MapLocation
-                        location={{...location}}
-                        variants={siteAnimation}
-                        header={header}
-                        description={description}
-                    />
-                    })
-                    }
-                </motion.div>
+          variants={container}
+          initial="hidden"
+          animate="show"
+          onAnimationComplete={() => setAnimating(!animating)}
+          className="hidden md:block"
+        >
+          {
+            Location.sort(() => Math.random() - 0.5).map(({ location, header, description }) => {
+              return <MapLocation
+                location={{ ...location }}
+                variants={siteAnimation}
+                header={header}
+                description={description}
+              />
+            })
+          }
+        </motion.div>
       </div>
       <div className="relative z-10 ">
         <div className="max-w-5xl mx-auto grid md:grid-cols-2  px-4">
@@ -85,26 +85,26 @@ export default function About({
 
 const InfoBox = ({ header, description }) => (
   <div className="p-2 bg-white w-48 rounded-md shadow-xl relative z-10">
-      <h4 className="text-xs font-bold">{header}</h4>
-      <p className="leading-tight text-xs">{description}</p>
+    <h4 className="text-xs font-bold">{header}</h4>
+    <p className="leading-tight text-xs">{description}</p>
   </div>
 )
 
 const MapLocation = ({ variants, header, description, location, animating }) =>
   <motion.div
-      variants={variants}
-      animate={animating ? "show" : null}
-      style={location}
-      onAnimationComplete={() => console.log("3")}
-      className="absolute">
-      <InfoBox
-          header={header}
-          description={description}
-      />
+    variants={variants}
+    animate={animating ? "show" : null}
+    style={location}
+    onAnimationComplete={() => console.log("3")}
+    className="absolute">
+    <InfoBox
+      header={header}
+      description={description}
+    />
 
-      <div className="w-full ">
-          <Pointer className="w-full -mt-2" />
-      </div>
+    <div className="w-full ">
+      <Pointer className="w-full -mt-2" />
+    </div>
 
 
   </motion.div>
